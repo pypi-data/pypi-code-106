8fae1390653ab7f64e0a3eb0d4409b45662bc811
Tom Forbes <tom@tomforb.es>
2022-01-01 12:07:03 +0000

    {"name":"as-dev-essense-zh","version":"2022.1.1.1","file":"as-dev-essense-zh-2022.1.1.1.tar.gz","path":"as-dev-essense-zh/as-dev-essense-zh-2022.1.1.1.tar.gz"}

---
+++ as-dev-essense-zh/as-dev-essense-zh-2022.1.1.1.tar.gz/AsDevEssenseZh/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""as-dev-essense-zh
+https://github.com/apachecn/as-dev-essense-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.1.1.1"
