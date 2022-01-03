818e25067bd1cabf315f1013d5a5dabb19fed542
Tom Forbes <tom@tomforb.es>
2022-01-03 07:53:08 +0000

    {"name":"geeksforgeeks-engi-zh","version":"2022.1.3.0","file":"geeksforgeeks-engi-zh-2022.1.3.0.tar.gz","path":"geeksforgeeks-engi-zh/geeksforgeeks-engi-zh-2022.1.3.0.tar.gz"}

---
+++ geeksforgeeks-engi-zh/geeksforgeeks-engi-zh-2022.1.3.0.tar.gz/GeeksforgeeksEngiZh/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""geeksforgeeks-engi-zh
+https://github.com/apachecn/geeksforgeeks-engi-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.1.3.0"
