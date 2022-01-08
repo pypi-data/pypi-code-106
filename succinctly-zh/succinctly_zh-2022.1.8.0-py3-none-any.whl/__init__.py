cb2ac2977c9591299925188508287ed19d0cb5db
Tom Forbes <tom@tomforb.es>
2022-01-08 11:53:19 +0000

    {"name":"succinctly-zh","version":"2022.1.8.0","file":"succinctly_zh-2022.1.8.0-py3-none-any.whl","path":"succinctly-zh/succinctly_zh-2022.1.8.0-py3-none-any.whl"}

---
+++ succinctly-zh/succinctly_zh-2022.1.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""succinctly-zh
+https://github.com/apachecn/succinctly-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.1.8.0"
